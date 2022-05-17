5289df43840ef1b87d6d7e49b0db9b4f9a86b0de
David Whipple <dwhipp88@gmail.com>
2022-05-17 17:16:35 -0600

    completed section 6 - 50

---
+++ app.js
@@ -0,0 +1,19 @@
+// normally you keep all the express configuration in app.js
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint now');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
