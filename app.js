9eb119d4deb3e0527b4d7441450e668ae447b6bf
David Whipple <dwhipp88@gmail.com>
2022-05-17 18:32:21 -0600

    completed Section 6 - 52

--- app.js
+++ app.js
@@ -6,19 +6,33 @@
 // 3: Use HTTP methods (verbs) - CRUD - POST, GET, PUT, PATCH, DELETE
 // 4: Send data as JSON (usually)
 // 5: Be stateless
-
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint now');
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint now');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
