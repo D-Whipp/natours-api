471097654b6cac6292b2d7c4e446f1b0f0314de6
David Whipple <dwhipp88@gmail.com>
2022-05-17 19:04:26 -0600

    completed Section 6 - 53

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const express = require('express');
 
 const app = express();
 
+// middleware
+// middleware is a function that can modify incoming data
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -33,6 +37,29 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //   console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+
+  // res.send('Done');
 });
 
 const port = 3000;
