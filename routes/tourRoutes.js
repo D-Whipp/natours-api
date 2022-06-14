91db20f12b68ea2ead2ff0425ac4e93fd08806eb
David Whipple <dwhipp88@gmail.com>
2022-06-13 20:03:53 -0600

    completed 88

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,7 +10,7 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', tourController.checkID);
+// router.param('id', tourController.checkID);
 
 // Create a checkBody middleware
 // Check if the body contains the name and price properties
