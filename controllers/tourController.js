cbfd41795b2b9ff98f329134ea716b66c1b2fece
Mr Whipp <dwhipp88@gmail.com>
2022-06-15 00:01:39 -0600

    completed 92

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -90,8 +90,8 @@ exports.updateTour = async (req, res) => {
 
 exports.deleteTour = async (req, res) => {
   try {
-    const tour = await Tour.findByIdAndDelete(req.params.id, req.body);
-
+    // const tour = await Tour.findByIdAndDelete(req.params.id, req.body);
+    await Tour.findByIdAndDelete(req.params.id);
     res.status(204).json({
       status: 'success',
       data: null,
